60c8d1319defd9f108f5647f39b91d2ea63238e3
Andreas Wiedmer <wiedmer.a@gmail.com>
2018-03-19 21:06:51 +0100

    remove unused method SectorTransaction::GetHash with missing return

--- src/LLD/transaction.h
+++ src/LLD/transaction.h
@@ -91,13 +91,6 @@ namespace LLD
 			
 			return true;
 		}
-		
-		/* Hash to keep track of the Sector Transactions in the Transaction History Journal. */
-		uint512 GetHash()
-        {
-            
-        }
-		
 	};
 }
 
